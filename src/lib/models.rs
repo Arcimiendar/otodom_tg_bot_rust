5d2f86ee0f40d33b163895bf4ae3995a3906ad8c
arcimiendar <artimendor@yandex.ru>
2022-07-24 02:08:39 +0200

    impl format for appartment

--- src/lib/models.rs
+++ src/lib/models.rs
@@ -1,4 +1,6 @@
-use chrono::NaiveDateTime;
+use std::fmt;
+use std::fmt::Formatter;
+use chrono::{NaiveDateTime, Utc};
 use super::schema::appartment;
 use super::schema::user;
 
@@ -10,6 +12,26 @@ pub struct Appartment {
     pub name: Option<String>,
     pub rooms: Option<i32>,
     pub scrapped_at: Option<NaiveDateTime>,
+}
+
+fn ref_unwrap_or<'a, T>(option: &'a Option<T>, default: &'a T) -> &'a T {
+    option.as_ref().unwrap_or(default)
+}
+
+impl fmt::Display for Appartment {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", vec![
+            format!("{}", ref_unwrap_or(&self.name, &String::from(""))),
+            format!("price: {}", ref_unwrap_or(&self.price, &0)),
+            format!("czynsz: {}", ref_unwrap_or(&self.czynsz, &0)),
+            format!("rooms: {}", ref_unwrap_or(&self.rooms, &0)),
+            format!(
+                "when scrapped: {}",
+                ref_unwrap_or(&self.scrapped_at, &Utc::now().naive_utc())
+                    .format("%Y-%m-%d %H:%M:%S").to_string()
+            )
+        ].join("\n"))
+    }
 }
 
 #[derive(Debug, Insertable)]
--- src/scrapper/otodom_scrapper.rs
+++ src/scrapper/otodom_scrapper.rs
@@ -1,7 +1,10 @@
 use std::fs::File;
 use std::io::Read;
 use std::time::Duration;
+use chrono::Utc;
+use teloxide::prelude::*;
 use tokio::time::sleep;
+use crate::lib::models::Appartment;
 
 pub fn parse_url_file(filename: &str) -> std::io::Result<Vec<String>> {
     let mut file = File::open(filename)?;
@@ -16,8 +19,23 @@ pub fn parse_url_file(filename: &str) -> std::io::Result<Vec<String>> {
 }
 
 pub async fn parse_continuous(url: String) {
+    let bot = Bot::from_env().auto_send();
     loop {
-        println!("hello! {}", url);
+        let appartments = parse_appartments(&url).await;
+        for app in appartments.iter() {
+            bot.send_message(ChatId(123), format!("{}", app)).await.unwrap();
+        }
         sleep(Duration::from_secs(60)).await;
     }
+}
+
+async fn parse_appartments(url: &String) -> Box<Vec<Appartment>> {
+    Box::new(vec![Appartment {
+        id: 1,
+        price: Some(5),
+        czynsz: Some(5),
+        name: Some(String::from("asd")),
+        rooms: Some(5),
+        scrapped_at: Some(Utc::now().naive_utc())
+    }])
 }
